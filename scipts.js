7fdb0460bf7976b5fa04b9b29a84b3b19d6b24ab
LuizEdsonNeto <167120474+LuizEdsonNeto@users.noreply.github.com>
2024-08-31 16:30:51 -0300

    teste

--- scipts.js
+++ scipts.js
@@ -56,3 +56,13 @@ function updateUsernameDisplay() {
 window.onload = function() {
     updateUsernameDisplay();
 };
+// script.js
+
+document.addEventListener('DOMContentLoaded', function() {
+    const menuToggle = document.getElementById('menu-toggle');
+    const navMenu = document.querySelector('nav ul');
+
+    menuToggle.addEventListener('click', function() {
+        navMenu.classList.toggle('active');
+    });
+});
